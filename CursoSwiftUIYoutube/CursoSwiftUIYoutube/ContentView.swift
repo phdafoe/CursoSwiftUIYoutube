34cc1b3755a141fd30c562f154e1f2366022fe8c
phdafoe <phdafoe@gmail.com>
2021-05-12 18:28:24 +0200

    [WIP] - My Firts commit

---
+++ CursoSwiftUIYoutube/CursoSwiftUIYoutube/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CursoSwiftUIYoutube
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 12/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CursoSwiftUIYoutube/CursoSwiftUIYoutube/CursoSwiftUIYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoSwiftUIYoutubeApp.swift
+//  CursoSwiftUIYoutube
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 12/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CursoSwiftUIYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
